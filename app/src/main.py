c778ab812408155bc96a75730cc3577c99b40355
al <al@t470>
2024-12-04 11:25:08 -0300

    Stage info in lambda

--- app/src/main.py
+++ app/src/main.py
@@ -1,10 +1,16 @@
 import json
-import boto3
-
-ddb = boto3.client("dynamodb")
 
 def lambda_handler(event, context):
+    print(event)
+    print(context)
+    if event is not None and \
+       'stageVariables' in event and \
+       'STAGE' in event['stageVariables']:
+            return {
+                "statusCode": 200,
+                "body": f"Hello world from {event['stageVariables']['STAGE']}!"
+            }
     return {
         "statusCode": 200,
-        "body": f"Hello world! How to get the stage name?"
+        "body": f"Hello world! No info for stage, sorry..."
     }
